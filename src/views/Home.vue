581d65e1975464ab4f371982452954de1c59bf32
Fernando Poli <fernandoandrespoli@gmail.com>
2019-03-09 17:04:52 -0300

    First Commit with everything.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,131 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <b-container fluid>
+    <!-- User Interface controls -->
+    <b-row>
+      <b-col md="6" class="my-1">
+        <b-form-group label-cols-sm="3" label="Filter" class="mb-0">
+          <b-input-group>
+            <b-form-input v-model="filter" placeholder="Type to Search" />
+            <b-input-group-append>
+              <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+            </b-input-group-append>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+
+      <b-col md="6" class="my-1">
+        <b-form-group label-cols-sm="3" label="Sort" class="mb-0">
+          <b-input-group>
+            <b-form-select v-model="sortBy" :options="sortOptions">
+              <option slot="first" :value="null">-- none --</option>
+            </b-form-select>
+            <b-form-select :disabled="!sortBy" v-model="sortDesc" slot="append">
+              <option :value="false">Asc</option> <option :value="true">Desc</option>
+            </b-form-select>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+<!--
+      <b-col md="6" class="my-1 mb-4">
+        <b-form-group label-cols-sm="3" label="Sort direction" class="mb-0">
+          <b-input-group>
+            <b-form-select v-model="sortDirection" slot="append">
+              <option value="asc">Asc</option> <option value="desc">Desc</option>
+              <option value="last">Last</option>
+            </b-form-select>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+
+      -->
+
+      <b-col md="6" class="my-1 mb-4">
+        <b-form-group label-cols-sm="3" label="Per page" class="mb-0">
+          <b-form-select :options="pageOptions" v-model="perPage" />
+        </b-form-group>
+      </b-col>
+    </b-row>
+
+    <!-- Main table element -->
+    <b-table bordered
+      show-empty
+      stacked="md"
+      :items="items"
+      :fields="fields"
+      :current-page="currentPage"
+      :per-page="perPage"
+      :filter="filter"
+      :sort-by.sync="sortBy"
+      :sort-desc.sync="sortDesc"
+      :sort-direction="sortDirection"
+      @filtered="onFiltered"
+    >
+
+    </b-table>
+
+    <b-row>
+      <b-col md="6" class="my-1">
+        <b-pagination
+          :total-rows="totalRows"
+          :per-page="perPage"
+          v-model="currentPage"
+          class="my-0"
+        />
+      </b-col>
+    </b-row>
+
+  </b-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+  import axios from 'axios'
 
-export default {
-  name: "home",
-  components: {
-    HelloWorld
+  const items = []
+
+  export default {
+    data() {
+      return {
+        items: [],
+        fields: [
+          { key: 'name', label: 'Planet name', sortable: true },
+          { key: 'diameter', label: 'Diameter', sortable: true },
+          { key: 'gravity', label: 'Gravity', sortable: true },
+          { key: 'climate', label: 'Climate', sortable: true }
+        ],
+        currentPage: 1,
+        perPage: 5,
+        totalRows: items.length,
+        pageOptions: [2, 5, 15],
+        sortBy: null,
+        sortDesc: false,
+        sortDirection: 'asc',
+        filter: null
+      }
+    },
+    created(){
+      axios.get('https://swapi.co/api/planets/?format=json')
+      .then(response => {
+         this.items = response.data.results
+      }) 
+      .catch(error => console.log(error))
+    },
+    computed: {
+      sortOptions() {
+        // Create an options list from our fields
+        return this.fields
+          .filter(f => f.sortable)
+          .map(f => {
+            return { text: f.label, value: f.key }
+          })
+      }
+    },
+    methods: {
+      onFiltered(filteredItems) {
+        // Trigger pagination to update the number of buttons/pages due to filtering
+        console.log(totalRows)
+        this.totalRows = filteredItems.length
+        this.currentPage = 1
+      }
+    }
   }
-};
 </script>
